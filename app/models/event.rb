e5b60ed788c085ae00455744211f8088bff28854
vicmaburrito <vmaldaraca@gmail.com>
2021-07-01 10:44:39 -0500

    Create time scopes, separated past & future events, clean views up

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: 'User'
     has_many :event_attendances, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendances
+    scope :past, -> { where('date > ?', DateTime.now) }
+    scope :future, -> { where('date <= ?', DateTime.now) }
 end
