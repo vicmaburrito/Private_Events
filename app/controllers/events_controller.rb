7f57e7aac305cf6279019791028dc3dc7db2ffd7
vicmaburrito <vmaldaraca@gmail.com>
2021-07-01 00:21:15 -0500

    Add logic between event, user & attendance models

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,7 @@ class EventsController < ApplicationController
   # GET /events or /events.json
   def index
     @events = Event.all
+    @event_attendances = EventAttendance.new
   end
 
   # GET /events/1 or /events/1.json
--- app/helpers/event_attendances_helper.rb
+++ app/helpers/event_attendances_helper.rb
@@ -1,2 +1,13 @@
 module EventAttendancesHelper
+    def event_form(current_event, event_attendance)
+      form_with(model: event_attendance) do |form|
+        content_tag(:div, class: 'main_form') do
+          form.hidden_field :attended_event_id, value: current_event.id
+        end
+        content_tag(:div, class: 'actions') do
+          form.submit 'attend this event'
+        end
+      end
+    end
+  end
 end
