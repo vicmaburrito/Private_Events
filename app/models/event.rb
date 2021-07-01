c0f527dca96d21b286ed00222aa1cd00ab6ec89c
vicmaburrito <vmaldaraca@gmail.com>
2021-07-01 14:21:58 -0500

    Fix rspec

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances
+  validates :title, presence: true, length: { maximum: 15 }
+  validates :body, presence: true, length: { minimum: 5 }
+  validates :date, presence: true
   scope :past, -> { where('date < ?', DateTime.now) }
   scope :future, -> { where('date >= ?', DateTime.now) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'validation test' do
     it 'ensures the username presence' do
-      user= User.new(email: 'admin@example.com', password: 1234567).save
+      user= User.new(email: 'klaus@email.com', password: 1234567).save
       expect(user).to eq(false)
     end
     it 'ensures the email presence' do
-      user= User.new(username: 'admin', password: 1234567).save
+      user= User.new(username: 'klaus', password: 1234567).save
       expect(user).to eq(false)
     end
     it 'ensures the password presence' do
-      user = User.new(username: 'admin', email: 'admin@example.com').save
+      user = User.new(username: 'klaus', email: 'klaus@email.com').save
       expect(user).to eq(false)
     end
     it 'should save successfully' do
-      user = User.new(username: 'admin', email: 'admin@example.com', password: 1234567).save
+      user = User.new(username: 'klaus', email: 'klaus@email.com', password: 1234567).save
       expect(user).to eq(true)
     end
   end
