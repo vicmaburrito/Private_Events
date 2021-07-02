491e1267c8802022acdf0d6bfdd5867f4c1fe09e
vicmaburrito <vmaldaraca@gmail.com>
2021-07-02 12:18:57 -0500

    Add changes required

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendances
-  validates :title, presence: true, length: { maximum: 15 }
+  validates :title, presence: true, length: { maximum: 30 }
   validates :body, presence: true, length: { minimum: 5 }
   validates :date, presence: true
   scope :past, -> { where('date < ?', DateTime.now) }
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -18,26 +18,12 @@ RSpec.describe Event, type: :model do
       event = Event.new(body: 'hey', title: 'beach', date: 20 - 5 - 2021, creator_id: 1).save
       expect(event).to eq false
     end
-    it 'ensures the length of the title has 15 maximum' do
+    it 'ensures the length of the title has 30 maximum' do
       event = Event.new(body: 'party on the beach',
                         title: 'the greates beach ever on humanity!', date: 20 - 5 - 2021, creator_id: 1).save
       expect(event).to eq false
     end
   end
-
-  # context 'scopes test' do
-  #   let (:params) { {title: 'beach party', body: 'party on the beach!', date: 2000-03-03, creator_id: 1 } }
-  #   before(:each) do
-  #     user1 = Event.new(params).save
-  #     user2 = Event.new(params).save
-  #     user3 = Event.new(params).save
-  #     user4 = Event.new(params.merge(date: 2000-03-03)).save
-  #     user5 = Event.new(params.merge(date: 2000-03-03)).save
-  #   end
-  #   it 'should return the events with an older date than now' do
-  #     expect(Event.past.size).to eql(3)
-  #   end
-  # end
 end
 
 RSpec.describe Event, type: :model do
